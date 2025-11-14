a935c7f5bbd246e5ad1ccd7d78aedb5048c2b725
FilipeCampos25 <lipecduarte255@gmail.com>
2025-11-14 20:15:40 -0300

    Adicionei o Banco de dados + outras configurações
    model/Adotante.java
    repository/AdotanteRepository.java
    service/AdotanteService.java
    
    configurei o templates adicionando o index que havia sumido.
    modifiquei o application.properties.
    modifiquei WebController.java

---
+++ src/main/java/com/adotematch/ai/model/Adotante.java
@@ -0,0 +1,39 @@
+package com.adotematch.ai.model;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Table(name = "adotante", schema = "adocao_animais")
+@Data
+@Builder
+public class Adotante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_adotante")
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private String residencia;
+
+    private String telefone;
+
+    private String email;
+
+    @Column(nullable = false)
+    private String endereco;
+
+    @Column(name = "experiencia_pets")
+    private String experienciaPets;
+
+    @Column(name = "espaco_em_casa")
+    private int espacoEmCasa;
+
+    @Column(name = "tempo_disponivel")
+    private int tempoDisponivel;
+
+    private String senha;
+}
---
+++ src/main/java/com/adotematch/ai/repository/AdotanteRepository.java
@@ -0,0 +1,8 @@
+package com.adotematch.ai.repository;
+
+import com.adotematch.ai.model.Adotante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdotanteRepository extends JpaRepository<Adotante, Long> {
+    Adotante findByEmail(String email);
+}
