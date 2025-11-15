0569cc87631e011308d66b4b05dc3c562f1c4d9e
FilipeCampos25 <lipecduarte255@gmail.com>
2025-11-14 22:51:28 -0300

    mudança nas rotas
    mudança nas rotas

--- src/main/java/com/adotematch/ai/repository/AdotanteRepository.java
+++ src/main/java/com/adotematch/ai/repository/AdotanteRepository.java
@@ -6,4 +6,3 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AdotanteRepository extends JpaRepository<Adotante, Long> {
     Adotante findByEmail(String email);
 }
-
